97eff3cf74cedd940df61032a4f0eb652cce73ed
leopro <zhirnowl@gmail.com>
2025-11-22 13:00:49 +0300

    сделал код невесь

--- main.py
+++ main.py
@@ -1,71 +1,49 @@
 import sys
+import sqlite3
+from PyQt6 import QtWidgets, uic
+from PyQt6.QtWidgets import QTableWidgetItem, QHeaderView
+from PyQt6.QtCore import Qt
 
-import io
-from PyQt6 import uic  # Импортируем uic
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtGui import QPainter, QColor
-import random
-template = """<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>Form</class>
- <widget class="QWidget" name="Form">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>1021</width>
-    <height>761</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>Form</string>
-  </property>
-  <widget class="QPushButton" name="pushButton">
-   <property name="geometry">
-    <rect>
-     <x>0</x>
-     <y>700</y>
-     <width>231</width>
-     <height>61</height>
-    </rect>
-   </property>
-   <property name="text">
-    <string>PushButton</string>
-   </property>
-  </widget>
- </widget>
- <resources/>
- <connections/>
-</ui>
-"""
-
-class MyWidget(QMainWindow):
+class CoffeeApp(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        f = io.StringIO(template)
-        uic.loadUi(f, self)
-        self.do_paint = False
-        self.pushButton.clicked.connect(self.paint)
+        uic.loadUi('main.ui', self)
+        
+        self.load_coffee_data()
+        self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        
+    def load_coffee_data(self):
+        try:
+            conn = sqlite3.connect('coffee.sqlite')
+            cursor = conn.cursor()
+            
+            cursor.execute('''
+                SELECT id, name, roast_level, bean_type, taste_description, price, package_volume
+                FROM coffee
+            ''')
+            
+            coffee_data = cursor.fetchall()
+            
+            self.tableWidget.setRowCount(len(coffee_data))
+            self.tableWidget.setColumnCount(7)
+            self.tableWidget.setHorizontalHeaderLabels([
+                'ID', 'Название', 'Степень обжарки', 'Тип', 'Описание вкуса', 'Цена', 'Объем упаковки'
+            ])
+            
+            for row, coffee in enumerate(coffee_data):
+                for col, value in enumerate(coffee):
+                    item = QTableWidgetItem(str(value))
+                    item.setFlags(item.flags() & ~Qt.ItemFlag.ItemIsEditable)
+                    self.tableWidget.setItem(row, col, item)
+            
+            conn.close()
+            
+        except sqlite3.Error as e:
+            print(f"Ошибка базы данных: {e}")
 
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter()
-            qp.begin(self)
-            self.draw_flag(qp)
-            qp.end()
-        self.do_paint = False
-
-    def paint(self):
-        self.do_paint = True
-        self.update()
-
-    def draw_flag(self, qp: QPainter):
-        qp.setPen(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-        x = random.randint(100, 200)
-        qp.drawEllipse(150, 150, x, x)
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
+    app = QtWidgets.QApplication(sys.argv)
+    window = CoffeeApp()
+    window.show()
     sys.exit(app.exec())
